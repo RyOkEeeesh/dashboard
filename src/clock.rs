3cd34bb249f4b71ec82ccf61b5a3f063274f2fca
kaji <169122209+RyOkEeeesh@users.noreply.github.com>
2026-04-09 17:00:32 +0900

    clock.rs を更新

--- src/clock.rs
+++ src/clock.rs
@@ -1,27 +1,25 @@
 use bevy::prelude::*;
 use chrono::Timelike;
 
-#[derive(Message)]
+#[derive(EntityEvent)]
 pub struct SecondTick(pub u32);
 
 pub struct ClockSysPlugin;
 
 impl Plugin for ClockSysPlugin {
     fn build(&self, app: &mut App) {
-        app.add_message::<SecondTick>()
-            .add_systems(Update, clock_system);
+        app.add_systems(Update, clock_system);
     }
 }
 
 fn clock_system(
     mut last_sec: Local<Option<u32>>, 
-    mut message_writer: MessageWriter<SecondTick>,
+    mut commands: Commands,
 ) {
     let now = chrono::Local::now().second();
 
     if Some(now) != *last_sec {
         *last_sec = Some(now);
-        message_writer.write(SecondTick(now));
-        println!("{now}");
+        commands.trigger(SecondTick(now));
     }
 }
