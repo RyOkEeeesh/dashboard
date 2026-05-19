4adfae74e27432436be9a59edaa36675ca5fc63d
kaji <dafe00842202@outlook.jp>
2026-05-19 18:40:21 +0900

    update

--- src/home.rs
+++ src/home.rs
@@ -1,6 +1,6 @@
-use std::collections::{HashMap, HashSet, hash_map::Entry};
+use std::{collections::{HashMap, HashSet}, rc::Rc};
 
-use slint::{ComponentHandle, Model};
+use slint::{ComponentHandle, Model, VecModel};
 use tokio::sync::{mpsc, oneshot};
 
 use crate::{
@@ -10,91 +10,80 @@ use crate::{
 };
 
 pub async fn setup_home(tx: mpsc::Sender<DbRequest>, ui: &Main) {
-    let (tx_oneshot, rx_oneshot) = oneshot::channel::<Vec<app_slot::Model>>();
+let (tx_oneshot, rx_oneshot) = oneshot::channel::<Vec<app_slot::Model>>();
     tx.send(DbRequest::GetApps(tx_oneshot)).await.unwrap();
 
     let app_db = rx_oneshot.await.unwrap();
-    let mut apps: Vec<AppData> = ui.global::<AppStates>().get_apps().iter().collect();
+    let ui_apps = ui.global::<AppStates>().get_apps();
 
     let mut app_map: HashMap<i32, String> = HashMap::new();
     let mut app_type_map: HashSet<String> = HashSet::new();
-    let mut i = 0;
 
     for app in app_db {
         if app_type_map.insert(app.app_name.clone()) {
-            app_map.entry(app.slot).or_insert(app.app_name);
+            app_map.insert(app.slot, app.app_name);
         }
     }
 
-    for app in &mut apps {
-        if app.app_type != AppTypes::None {
+    let mut next_slot = 0;
+    for app in ui_apps.iter() {
+        if app.app_type == AppTypes::None {
             continue;
         }
         let val = format!("{:?}", app.app_type);
+        
         if app_type_map.insert(val.clone()) {
-            loop {
-                match app_map.entry(i) {
-                    Entry::Vacant(entry) => {
-                        entry.insert(val);
-                        break;
-                    }
-                    Entry::Occupied(_) => {
-                        i += 1;
-                    }
-                }
+            while app_map.contains_key(&next_slot) {
+                next_slot += 1;
             }
+            app_map.insert(next_slot, val);
+            next_slot += 1;
         }
     }
 
     let app_limit = ui.global::<AppStates>().get_app_limit();
     let max_slot = app_map.keys().max().copied().unwrap_or(0);
     let total_pages = (max_slot / app_limit) + 1;
+    
     let mut page_status = vec![false; total_pages as usize];
-
-    for num in app_map.keys() {
-        page_status[(num / app_limit) as usize] = true;
+    for &slot in app_map.keys() {
+        page_status[(slot / app_limit) as usize] = true;
     }
 
+    let mut final_apps: Vec<AppData> = Vec::new();
     let mut empty_pages = 0;
-    let mut apps: Vec<AppData> = Vec::new();
 
-    if page_status.contains(&false) {
-        for i in 0..total_pages {
-            if page_status[i as usize] {
-                if empty_pages > 0 {
-                    for slot in (app_limit * i)..(app_limit * (i + 1)) {
-                        if let Some((_, v)) = app_map.remove_entry(&slot) {
-                            if let Some(app_type) = get_app_type(&v) {
-                                apps.push(AppData {
-                                    app_type,
-                                    slot: slot - app_limit * empty_pages,
-                                });
-                            }
-                        }
-                    }
-                } else {
-                    for slot in (app_limit * i)..(app_limit * (i + 1)) {
-                        if let Some((_, v)) = app_map.remove_entry(&slot) {
-                            if let Some(app_type) = get_app_type(&v) {
-                                apps.push(AppData { app_type, slot });
-                            }
-                        }
+    for page_idx in 0..total_pages {
+        if page_status[page_idx as usize] {
+            let start_slot = app_limit * page_idx;
+            let end_slot = app_limit * (page_idx + 1);
+            
+            for slot in start_slot..end_slot {
+                if let Some(v) = app_map.remove(&slot) {
+                    if let Some(app_type) = get_app_type(&v) {
+                        let new_slot = slot - (app_limit * empty_pages);
+                        final_apps.push(AppData { app_type, slot: new_slot });
                     }
                 }
-            } else {
-                empty_pages += 1;
             }
-        }
-    } else {
-        for (slot, app_type) in app_map.iter() {
-            //　ここから
+        } else {
+            empty_pages += 1;
         }
     }
+
+    let active_pages = page_status.iter().filter(|&&x| x).count();
+    let page_num = if active_pages > 0 { active_pages - 1 } else { 0 };
+
+    ui.global::<AppStates>().set_page_num(page_num as i32);
+    ui.global::<AppStates>().set_apps(Rc::new(VecModel::from(final_apps)).into());
+
 }
 
 fn get_app_type(str: &String) -> Option<AppTypes> {
     if *str == format!("{:?}", AppTypes::RoomTemp) {
         return Some(AppTypes::RoomTemp);
+    } else if *str == format!("{:?}", AppTypes::Other) {
+        return Some(AppTypes::Other);
     }
     None
 }
